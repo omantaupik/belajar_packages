b0961f4b631843c2eabf974900e2d08d0f720ab3
oman taupik <omantaupik3419@gmail.com>
2024-06-25 12:48:27 +0700

    dart library

---
+++ example/hello.dart
@@ -0,0 +1,9 @@
+import 'package:belajar_packages/hello.dart';
+
+void main(){
+  print(sayHello('oman'));
+
+  // var customer = Customer();
+  // var product = Product();
+  // var category = Category();
+}
---
+++ lib/hello.dart
@@ -0,0 +1,5 @@
+library belajar_packages;
+
+export 'src/customer.dart' show Customer, Category, Product;
+
+export 'src/say_hello.dart';
